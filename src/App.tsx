d86ac2396a2b577ec5fe48eb1304d39705dff618
Roger Bernardo de Melo Lima <rbmelolima@gmail.com>
2020-08-19 14:39:24 -0300

    feat: implementação do dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,41 @@
 import 'react-calendar-heatmap/dist/styles.css';
-
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
-
 import Profile from './pages/Profile';
 import Repository from './pages/Repository';
-
 import GlobalStyles from './styles/GlobalStyles';
+import { ThemeName, themes } from './styles/theme';
 
 function App() {
+  const [ themeName, setThemeName ] = useState<ThemeName>('light');
+  const currentTheme = themes[ themeName ];
+
   return (
-    <BrowserRouter>
-      <Header />
-
-      <Routes>
-        <Route
-          path="/"
-          element={ <Profile /> }
-        />
-        <Route
-          path="/:username"
-          element={ <Profile /> }
-        />
-        <Route
-          path="/:username/:repository"
-          element={ <Repository /> }
-        />
-      </Routes>
-      
-      <Footer />
-
-      <GlobalStyles />
-    </BrowserRouter>
+    <ThemeProvider theme={ currentTheme }>
+      <BrowserRouter>
+        <Header themeName={ themeName } setThemeName={ setThemeName } />
+        <Routes>
+          <Route
+            path="/"
+            element={ <Profile /> }
+          />
+          <Route
+            path="/:username"
+            element={ <Profile /> }
+          />
+          <Route
+            path="/:username/:repository"
+            element={ <Repository /> }
+          />
+        </Routes>
+        <Footer />
+        <GlobalStyles />
+      </BrowserRouter>
+    </ThemeProvider>
   );
 };
 
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,14 @@
 import React, { useState } from 'react';
 import { Container, GithubLogo, SearchForm } from './styles';
 import { useNavigate } from 'react-router-dom';
+import { ThemeName } from '../../styles/theme';
 
-const Header: React.FC = () => {
+interface Props {
+  themeName: ThemeName;
+  setThemeName: (newName: ThemeName) => void;
+}
+
+const Header: React.FC<Props> = ({ themeName, setThemeName }) => {
   const [ search, setSearch ] = useState('');
   const navigate = useNavigate();
 
@@ -11,9 +17,13 @@ const Header: React.FC = () => {
     navigate('/' + search.toLowerCase().trim());
   }
 
+  function toggleTheme() {
+    setThemeName(themeName === 'light' ? 'dark' : 'light');
+  }
+
   return (
     <Container>
-      <GithubLogo />
+      <GithubLogo onClick={ toggleTheme } />
       <SearchForm onSubmit={ handleSubmit }>
         <input
           value={ search }
