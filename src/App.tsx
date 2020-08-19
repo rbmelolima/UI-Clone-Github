07f5171a6c13f1d2b139a2f6fadebc81b9fbc08f
Roger Bernardo de Melo Lima <rbmelolima@gmail.com>
2020-08-19 14:10:52 -0300

    feat: criação do footer

--- src/App.tsx
+++ src/App.tsx
@@ -30,6 +30,7 @@ function App() {
           element={ <Repository /> }
         />
       </Routes>
+      
       <Footer />
 
       <GlobalStyles />
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,25 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 import { Container, GithubLogo, SearchForm } from './styles';
+import { useNavigate } from 'react-router-dom';
 
 const Header: React.FC = () => {
+  const [ search, setSearch ] = useState('');
+  const navigate = useNavigate();
+
+  function handleSubmit(event: React.FormEvent) {
+    event.preventDefault();
+    navigate('/' + search.toLowerCase().trim());
+  }
+
   return (
     <Container>
       <GithubLogo />
-      <SearchForm>
-        <input placeholder="Enter username or repository..."/>
+      <SearchForm onSubmit={ handleSubmit }>
+        <input
+          value={ search }
+          onChange={ e => setSearch(e.currentTarget.value) }
+          placeholder="Enter username or repository..."
+        />
       </SearchForm>
     </Container>
   );
